0380b5f158111c49e0c87818b6405d18c7a74176
Phil Curran <pecurran@outlook.com>
2024-03-25 16:41:42 -0700

    tweaks to frontend; hooked up forms; prep to push to remote.

--- src/pages/Gateway/Login.jsx
+++ src/pages/Gateway/Login.jsx
@@ -15,6 +15,7 @@ import {
   Button,
   InputGroup,
   InputLeftElement,
+  FormControl,
 } from "@chakra-ui/react";
 import { EmailIcon, LockIcon, UnlockIcon } from "@chakra-ui/icons";
 import "./gateway.css";
@@ -31,84 +32,88 @@ const Gateway = () => {
     setEmail(event.target.value);
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     console.log(email, password);
   };
 
   return (
-    <div className="register container">
+    <div className="login container">
       <Card className="middle">
-        <CardBody>
-          <VStack spacing={6}>
-            <Box>
-              <Center>
-                <Heading as="h1" size="lg">
-                  Login
-                </Heading>
-              </Center>
-            </Box>
-            <Box width="80%">
-              <InputGroup>
-                <InputLeftElement pointerEvents="none">
-                  {email === "" ? (
-                    <EmailIcon boxSize={4} color="black5" />
-                  ) : validator.isEmail(email) ? (
-                    <EmailIcon boxSize={5} color="lightGreen" />
-                  ) : (
-                    <EmailIcon boxSize={5} color="brightYellow" />
-                  )}
-                </InputLeftElement>
-                <Input
-                  onChange={handleEmailChange}
-                  variant="flushed"
-                  type="email"
-                  placeholder="Email"
-                />
-              </InputGroup>
-            </Box>
-            <Box width="80%">
-              <InputGroup>
-                <InputLeftElement pointerEvents="none">
-                  {password === "" ? (
-                    <UnlockIcon boxSize={4} color="black5" />
-                  ) : (
-                    <LockIcon
-                      boxSize={5}
-                      color={
-                        validator.isStrongPassword(password)
-                          ? "lightGreen"
-                          : "brightYellow"
-                      }
-                    />
-                  )}
-                </InputLeftElement>
-                <Input
-                  onChange={handlePasswordChange}
-                  variant="flushed"
-                  type="password"
-                  placeholder="Password"
-                />
-              </InputGroup>
-            </Box>
-            <Box width="80%">
-              <Button
-                isDisabled={email == "" || password == ""}
-                width="100%"
-                colorScheme="blue"
-              >
-                Log In
-              </Button>
-            </Box>
-            <Box>
-              <Text>
-                Need to register?{"  "}
-                <span className="link">
-                  <Link to="/register">Register here.</Link>
-                </span>
-              </Text>
-            </Box>
-          </VStack>
-        </CardBody>
+        <form onSubmit={handleSubmit}>
+          <CardBody>
+            <VStack spacing={6}>
+              <Box>
+                <Center>
+                  <Heading as="h1" size="lg">
+                    Login
+                  </Heading>
+                </Center>
+              </Box>
+              <Box width="80%">
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    {email === "" ? (
+                      <EmailIcon boxSize={4} color="black5" />
+                    ) : validator.isEmail(email) ? (
+                      <EmailIcon boxSize={5} color="lightGreen" />
+                    ) : (
+                      <EmailIcon boxSize={5} color="brightYellow" />
+                    )}
+                  </InputLeftElement>
+                  <Input
+                    onChange={handleEmailChange}
+                    variant="flushed"
+                    type="email"
+                    placeholder="Email"
+                  />
+                </InputGroup>
+              </Box>
+              <Box width="80%">
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    {password === "" ? (
+                      <UnlockIcon boxSize={4} color="black5" />
+                    ) : (
+                      <LockIcon
+                        boxSize={5}
+                        color={
+                          validator.isStrongPassword(password)
+                            ? "lightGreen"
+                            : "brightYellow"
+                        }
+                      />
+                    )}
+                  </InputLeftElement>
+                  <Input
+                    onChange={handlePasswordChange}
+                    variant="flushed"
+                    type="password"
+                    placeholder="Password"
+                  />
+                </InputGroup>
+              </Box>
+              <Box width="80%">
+                <Button
+                  isDisabled={email == "" || password == ""}
+                  width="100%"
+                  colorScheme="blue"
+                  type="submit"
+                >
+                  Log In
+                </Button>
+              </Box>
+              <Box>
+                <Text>
+                  Need to register?{"  "}
+                  <span className="link">
+                    <Link to="/register">Register here.</Link>
+                  </span>
+                </Text>
+              </Box>
+            </VStack>
+          </CardBody>
+        </form>
       </Card>
     </div>
   );
--- src/pages/Gateway/Register.jsx
+++ src/pages/Gateway/Register.jsx
@@ -21,9 +21,13 @@ import { EmailIcon, LockIcon, UnlockIcon } from "@chakra-ui/icons";
 import "./gateway.css";
 
 const Gateway = () => {
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [email, setEmail] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
@@ -33,111 +37,115 @@ const Gateway = () => {
     setConfirmPassword(event.target.value);
   };
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email, password, confirmPassword);
   };
 
   return (
     <div className="register container">
       <Card className="middle">
-        <CardBody>
-          <VStack spacing={6}>
-            <Box>
-              <Center>
-                <Heading as="h1" size="lg">
+        <form onSubmit={handleSubmit}>
+          <CardBody>
+            <VStack spacing={6}>
+              <Box>
+                <Center>
+                  <Heading as="h1" size="lg">
+                    Register
+                  </Heading>
+                </Center>
+              </Box>
+              <Box width="80%">
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    {email === "" ? (
+                      <EmailIcon boxSize={4} color="black5" />
+                    ) : validator.isEmail(email) ? (
+                      <EmailIcon boxSize={5} color="lightGreen" />
+                    ) : (
+                      <EmailIcon boxSize={5} color="brightYellow" />
+                    )}
+                  </InputLeftElement>
+                  <Input
+                    onChange={handleEmailChange}
+                    variant="flushed"
+                    type="email"
+                    placeholder="Email"
+                  />
+                </InputGroup>
+              </Box>
+              <Box width="80%">
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    {password === "" ? (
+                      <UnlockIcon boxSize={4} color="black5" />
+                    ) : (
+                      <LockIcon
+                        boxSize={5}
+                        color={
+                          validator.isStrongPassword(password)
+                            ? "lightGreen"
+                            : "brightYellow"
+                        }
+                      />
+                    )}
+                  </InputLeftElement>
+                  <Input
+                    onChange={handlePasswordChange}
+                    variant="flushed"
+                    type="password"
+                    placeholder="Password"
+                  />
+                </InputGroup>
+              </Box>
+              <Box width="80%">
+                <InputGroup>
+                  <InputLeftElement pointerEvents="none">
+                    {confirmPassword === "" ? (
+                      <UnlockIcon boxSize={4} color="black5" />
+                    ) : (
+                      <LockIcon
+                        boxSize={5}
+                        color={
+                          password === confirmPassword ? "lightGreen" : "red"
+                        }
+                      />
+                    )}
+                  </InputLeftElement>
+                  <Input
+                    onChange={handleConfirmPassword}
+                    variant="flushed"
+                    type="password"
+                    placeholder="Confirm Password"
+                  />
+                </InputGroup>
+              </Box>
+              <Box width="80%">
+                <Button
+                  isDisabled={
+                    email == "" ||
+                    password == "" ||
+                    confirmPassword == "" ||
+                    password !== confirmPassword
+                  }
+                  width="100%"
+                  colorScheme="blue"
+                  type="submit"
+                >
                   Register
-                </Heading>
-              </Center>
-            </Box>
-            <Box width="80%">
-              <InputGroup>
-                <InputLeftElement pointerEvents="none">
-                  {email === "" ? (
-                    <EmailIcon boxSize={4} color="black5" />
-                  ) : validator.isEmail(email) ? (
-                    <EmailIcon boxSize={5} color="lightGreen" />
-                  ) : (
-                    <EmailIcon boxSize={5} color="brightYellow" />
-                  )}
-                </InputLeftElement>
-                <Input
-                  onChange={handleEmailChange}
-                  variant="flushed"
-                  type="email"
-                  placeholder="Email"
-                />
-              </InputGroup>
-            </Box>
-            <Box width="80%">
-              <InputGroup>
-                <InputLeftElement pointerEvents="none">
-                  {password === "" ? (
-                    <UnlockIcon boxSize={4} color="black5" />
-                  ) : (
-                    <LockIcon
-                      boxSize={5}
-                      color={
-                        validator.isStrongPassword(password)
-                          ? "lightGreen"
-                          : "brightYellow"
-                      }
-                    />
-                  )}
-                </InputLeftElement>
-                <Input
-                  onChange={handlePasswordChange}
-                  variant="flushed"
-                  type="password"
-                  placeholder="Password"
-                />
-              </InputGroup>
-            </Box>
-            <Box width="80%">
-              <InputGroup>
-                <InputLeftElement pointerEvents="none">
-                  {confirmPassword === "" ? (
-                    <UnlockIcon boxSize={4} color="black5" />
-                  ) : (
-                    <LockIcon
-                      boxSize={5}
-                      color={
-                        password === confirmPassword ? "lightGreen" : "red"
-                      }
-                    />
-                  )}
-                </InputLeftElement>
-                <Input
-                  onChange={handleConfirmPassword}
-                  variant="flushed"
-                  type="password"
-                  placeholder="Confirm Password"
-                />
-              </InputGroup>
-            </Box>
-            <Box width="80%">
-              <Button
-                isDisabled={
-                  email == "" ||
-                  password == "" ||
-                  confirmPassword == "" ||
-                  password !== confirmPassword
-                }
-                width="100%"
-                colorScheme="blue"
-              >
-                Register
-              </Button>
-            </Box>
-            <Box>
-              <Text>
-                Already registered?{"  "}
-                <span className="link">
-                  <Link to="/login">Login here.</Link>
-                </span>
-              </Text>
-            </Box>
-          </VStack>
-        </CardBody>
+                </Button>
+              </Box>
+              <Box>
+                <Text>
+                  Already registered?{"  "}
+                  <span className="link">
+                    <Link to="/login">Login here.</Link>
+                  </span>
+                </Text>
+              </Box>
+            </VStack>
+          </CardBody>
+        </form>
       </Card>
     </div>
   );
